52d3aa42ae4115d2b24d2526e82f1ce528457caf
root <ucheogbonnak@gmail.com>
2023-01-03 14:05:43 -0800

    added

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,7 +14,7 @@ Return True if all boxes can be opened, else return False
 
 def canUnlockAll(boxes):
     """ method that determines if all the boxes can be opened """
-    newlist = []
+    newlist = [0]
     k = len(boxes)
     for i in boxes:
         if len(i) == 0 and i is not boxes[k-1]:
